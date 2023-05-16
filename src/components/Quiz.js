dcfb73a856a4c25ea7cbff08db23d1b560585d47
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-05-16 15:30:03 +0200

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,12 +4,22 @@ import {useState, useEffect} from "react"
 
 export default function Quiz(){
     const [allQuestions, setAllQuestions] = useState()
+    const [questions, setQuestions] = useState()
 
     useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
             .then(data => setAllQuestions(data.results))
     }, [])
+
+    useEffect(() => {
+        if(allQuestions){
+            const newArray = allQuestions.map(question => {
+                return {...question, randomAnswers: question.incorrect_answers.concat(question.correct_answer)}
+            })
+            console.log(newArray)
+        }
+    }, [allQuestions])
 
     return (
         <>
