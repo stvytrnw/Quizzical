3ba2b909c1d64ad4756816f1dc070184d9ae263f
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-05-22 00:03:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react"
 import {useState} from "react"
 import Start from "./components/Start"
 import Quiz from './components/Quiz';
-import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 
 function App() {
   const [startQuiz, setStartQuiz] = useState(false)
